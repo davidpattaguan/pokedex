393b1c0bf56ac5a857ea5d72a92a01882f83d42f
David Niño Angelo Pattaguan <57347782+davidpattaguan@users.noreply.github.com>
2021-09-17 08:29:08 +0800

    added lazy loading

--- src/components/PokemonsList.jsx
+++ src/components/PokemonsList.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import PokemonListItems from "./PokemonListItems";
+// import PokemonListItems from "./PokemonListItems";
+import { lazy, Suspense } from "react";
 import { useState } from "react";
+
+const PokemonListItems = lazy(() => import("./PokemonListItems"));
 
 const PokemonsList = ({ pokemons }) => {
   const [searchPoke, setSearchPoke] = useState("");
@@ -16,24 +19,26 @@ const PokemonsList = ({ pokemons }) => {
   }
   return (
     <div className>
-      <div className="mx-20 mb-5">
-        <input
-          type="text"
-          placeholder=" Search Pokemon"
-          className="rounded w-30"
-          onChange={(event) => handleSearch(event.target.value)}
-        />
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mx-20">
-        {pokemons.map((pokemon) => (
-          <PokemonListItems
-            key={pokemon.id}
-            id={pokemon.id}
-            name={pokemon.name}
-            sprite={pokemon.sprite}
+      <Suspense fallback={<h1>Still Loading…</h1>}>
+        <div className="mx-20 mb-5">
+          <input
+            type="text"
+            placeholder=" Search Pokemon"
+            className="rounded w-30"
+            onChange={(event) => handleSearch(event.target.value)}
           />
-        ))}
-      </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mx-20">
+          {pokemons.map((pokemon) => (
+            <PokemonListItems
+              key={pokemon.id}
+              id={pokemon.id}
+              name={pokemon.name}
+              sprite={pokemon.sprite}
+            />
+          ))}
+        </div>
+      </Suspense>
     </div>
   );
 };
