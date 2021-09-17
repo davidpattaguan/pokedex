6712a59d339f80762e371ec2e301d944235aaa1b
David Niño Angelo Pattaguan <57347782+davidpattaguan@users.noreply.github.com>
2021-09-17 08:46:21 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import useFetch from "./useFetch";
 
 function App() {
   const { data, loading, error } = useFetch(
-    "https://pokeapi.co/api/v2/pokemon?limit=500"
+    "https://pokeapi.co/api/v2/pokemon?limit=151"
   );
 
   return (
