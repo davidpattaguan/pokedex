2a005aa299694b53aeec2f95b1525aab4d7a3784
David Niño Angelo Pattaguan <57347782+davidpattaguan@users.noreply.github.com>
2021-09-17 08:51:23 +0800

    Lazy Loading V2

--- src/components/PokemonListItems.jsx
+++ src/components/PokemonListItems.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import Card from "../utilities/Card";
+// import Card from "../utilities/Card";
 import { lazy, Suspense } from "react";
+import Loading from "react-loading";
+
+const Card = lazy(() => import("../utilities/Card"));
 
 const PokemonListItems = ({ id, name, sprite }) => {
   return (
     <div>
-      <Card id={id} name={name} sprite={sprite} />
+      <Suspense fallback={<Loading />}>
+        <Card id={id} name={name} sprite={sprite} />
+      </Suspense>
     </div>
   );
 };
--- src/components/PokemonsList.jsx
+++ src/components/PokemonsList.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-// import PokemonListItems from "./PokemonListItems";
-import { lazy, Suspense } from "react";
-import { useState } from "react";
+import PokemonListItems from "./PokemonListItems";
 
-const PokemonListItems = lazy(() => import("./PokemonListItems"));
+import { useState } from "react";
 
 const PokemonsList = ({ pokemons }) => {
   const [searchPoke, setSearchPoke] = useState("");
@@ -19,26 +17,24 @@ const PokemonsList = ({ pokemons }) => {
   }
   return (
     <div className>
-      <Suspense fallback={<h1>Still Loading…</h1>}>
-        <div className="mx-20 mb-5">
-          <input
-            type="text"
-            placeholder=" Search Pokemon"
-            className="rounded w-30"
-            onChange={(event) => handleSearch(event.target.value)}
+      <div className="mx-20 mb-5">
+        <input
+          type="text"
+          placeholder=" Search Pokemon"
+          className="rounded w-30"
+          onChange={(event) => handleSearch(event.target.value)}
+        />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mx-20">
+        {pokemons.map((pokemon) => (
+          <PokemonListItems
+            key={pokemon.id}
+            id={pokemon.id}
+            name={pokemon.name}
+            sprite={pokemon.sprite}
           />
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mx-20">
-          {pokemons.map((pokemon) => (
-            <PokemonListItems
-              key={pokemon.id}
-              id={pokemon.id}
-              name={pokemon.name}
-              sprite={pokemon.sprite}
-            />
-          ))}
-        </div>
-      </Suspense>
+        ))}
+      </div>
     </div>
   );
 };
